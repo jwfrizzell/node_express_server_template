a64bb3aa0af2718c96ea41a7cced56c1bd694a9f
Joe Frizzell <joefrizzell@Joes-MacBook-Pro-2.local>
2018-08-13 15:13:38 -0400

    install cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router.js");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 //DB Setup
 mongoose.connect(
@@ -15,6 +16,7 @@ mongoose.connect(
 
 //App setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
